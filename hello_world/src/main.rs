48a43fcb3e0ab94cc31e64ef36898095a0e2cba1
aterlupus <rajraviupreti@email.com>
2026-06-03 18:37:33 +0200

    print and println

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     let name = "Raj";
     println!("Hello, {}! from 🦀", name);
     println!("I am learning Rust!");
-    println!("Never back down never what?");
+    print!("Never back down never what? "); // this needs to be print instead of println to avoid moving to the next line
+    print!("Never give up!"); // This will print on the same line as the previous print statement
 }
