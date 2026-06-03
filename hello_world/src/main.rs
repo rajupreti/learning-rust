6dbc2e6feced6e28abe1dcb942d3a3dcd3ae301f
aterlupus <rajraviupreti@email.com>
2026-06-03 18:42:55 +0200

    get rick rolled

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -8,5 +8,8 @@ fn main() {
     println!("I am learning Rust!");
     print!("Never back down never what? "); // this needs to be print instead of println to avoid moving to the next line
     print!("Never give up!"); // This will print on the same line as the previous print statement
-    print!("\nNever gonna give you up!"); // This will move to the next line after the previous print statements
+    print!("\nNever gonna give you up!\nNever gonna let you down!"); // This will move to the next line after the previous print statements
+    /*
+    Get rickrolled b*ch
+    */
 }
